04bbb3be1cd9d4b386c08a92d0f01475d2138d51
T. Sheng <34408561+tsheng0315@users.noreply.github.com>
2020-07-11 21:21:28 +0100

    Update 1470. Shuffle the Array.py

--- 1470. Shuffle the Array.py
+++ 1470. Shuffle the Array.py
@@ -1,3 +1,4 @@
+'''
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
@@ -21,7 +22,7 @@ nums.length == 2n
 
 ## Hint:
 用一个新的数组按照 nums[0], nums[n], nums[1], nums[n + 1], ... 的顺序依次保存所有的排列。
-
+'''
 # Solution
 class Solution:
     def shuffle(self,nums: List[int], n: int) -> List[int]:
