330f802cca83562221bb2dfe903390ebd4d71a6e
tsheng0315 <shengtingran@gmail.com>
2021-10-14 21:33:11 +0100

    fixed empty list

--- data_structure_python/select_sort.py
+++ data_structure_python/select_sort.py
@@ -10,12 +10,13 @@ def select_sort_simple(list):# O(n^2)
     for i in range(len(list)): # O(n)
         min_val = min(list) # O(n), 
         new_list.append(min_val)
-        list.remove(min_val) # O(n), as elements after deleted item need to be moved
+        list.remove(min_val) # O(n), as elements after deleted item need to be moved forward
     return new_list
 
+# improved version
 
-# I don't wish to get a new list->
-# find min, put it at the first position of the list, replace the original first item with the min 
+# I don't wish to get a new list-> as it adds space complexity 
+# find min, move it to the first position of the list, replace the original first item with this min value 
 def select_sort(list):
     for i in range(len(list)-1): #i是第几趟，-1 是因为 最后一个数就是最大的了，不用动。O(n)
         #遍历无序区
@@ -26,12 +27,11 @@ def select_sort(list):
         list[i], list[min_loc] = list[min_loc], list[i]
     return list
 
-            
-
-
 
 li =[3,2,4,5,6,2,6,1]
 print(select_sort_simple(li))
-print(select_sort(li))
+
+li2 =[3,2,4,5,6,2,6,1]
+print(select_sort(li2))
 
 
