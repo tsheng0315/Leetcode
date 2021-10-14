afac454b0fb07f547dad9556cbec5dd27f186823
tsheng0315 <shengtingran@gmail.com>
2021-10-14 14:31:33 +0100

    shortcut

--- data_structure_python/bubble sort.py
+++ data_structure_python/bubble sort.py
@@ -5,7 +5,7 @@ bubble sort
 import random
 def bubble(arr):
     for i in range(len(arr)-1):
-        for j in range(len(arr)-i-1):
+        for j in range(len(arr)-i-1): # pointer
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
@@ -13,3 +13,17 @@ def bubble(arr):
 list = [random.randint(0,10) for i in range(10)]
 print(list)
 print(bubble(list))
+
+# improvement: if no exchange happens in one iteration -> return 
+def bubble_imp(arr):
+    for i in range(len(arr)-1):
+        exchange = False
+        for j in range(len(arr)-i-1): # pointer
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                exchange =True
+        if not exchange:
+            return arr
+    return arr
+
+print(bubble_imp(list))
